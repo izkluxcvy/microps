4d83d06f49aed97ef550dfaa0b62c8b0a53f82f2
izkluxcvy <pettyblue@proton.me>
2026-05-09 20:59:06 +0900

    Completion of step 13

--- ip.c
+++ ip.c
@@ -11,6 +11,7 @@
 #include "net.h"
 #include "ip.h"
 #include "icmp.h"
+#include "arp.h"
 
 #define IP_HDR_FLAG_MF 0x2000 /* more flagments flag */
 #define IP_HDR_FLAG_DF 0x4000 /* don't flagment flag */
@@ -259,6 +260,7 @@ ip_output_device(struct ip_iface *iface, const uint8_t *data, size_t len, ip_add
 {
     char addr[IP_ADDR_STR_LEN];
     uint8_t hwaddr[NET_DEVICE_ADDR_LEN] = {};
+    int ret;
 
     ip_addr_ntop(target, addr, sizeof(addr));
     debugf("dev=%s, len=%zu, target=%s", NET_IFACE(iface)->dev->name, len, addr);
@@ -266,8 +268,10 @@ ip_output_device(struct ip_iface *iface, const uint8_t *data, size_t len, ip_add
         if (target == iface->broadcast || target == IP_ADDR_BROADCAST) {
             memcpy(hwaddr, NET_IFACE(iface)->dev->broadcast, NET_IFACE(iface)->dev->alen);
         } else {
-            errorf("ARP does not implement");
-            return -1;
+            ret = arp_resolve(NET_IFACE(iface), target, hwaddr);
+            if (ret != ARP_RESOLVE_FOUND) {
+                return ret;
+            }
         }
     }
     return net_device_output(NET_IFACE(iface)->dev, NET_PROTOCOL_TYPE_IP, data, len, hwaddr);
